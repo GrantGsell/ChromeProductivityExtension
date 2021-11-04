c90321d47d3ab4f64cc2992edbcb368258a6b331
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-11-03 22:36:29 -0400

    Changed JS code to jQuery

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-const contents = document.getElementById('contents');
-contents.parentNode.removeChild(contents);
+// JavaScript
+//const contents = document.getElementById('contents');
+//contents.parentNode.removeChild(contents);
+
+// jQuery code to simplify above JS code
+var contents = $('#contents');
+contents.remove();
