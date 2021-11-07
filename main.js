595609674b9a286c39b144f1189dd822a12c29c6
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-11-07 18:12:50 -0500

    Changed extension on/off functionality from checking values to checking storage for changes

--- main.js
+++ main.js
@@ -6,20 +6,25 @@
 var contents = $('#contents');
 var parent = $('#contents').parentElement;
 
-// Simple dynamic extension control 
-chrome.storage.local.get(['onOff', 'timeStart', 'timeEnd'], function(res){
-	var onFlag  = res.onOff;
 
-	// Check active times
-	var currTime = new Date().toString();
-	var currDate = currTime.slice(0,16);
-	var time0 = Date.parse(currDate + res.timeStart);
-	var time1 = Date.parse(currDate + res.timeEnd);
-	var newTime = Date.parse(currTime);
-	
-	if(onFlag && (time0 <= newTime && newTime <= time1)){
-		contents.remove();
-	}else{
-		parent.prepend(contents);
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+	for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+		console.log(
+			`Storage key "${key}" in namespace "${namespace}" changed.`,
+			`Old value was "${oldValue}", new value is "${newValue}".`
+		);
+		chrome.storage.local.get(['onOff', 'timeStart', 'timeEnd'], function(res){
+			console.log(res);
+			var currTime = new Date().toString();
+			var currDate = currTime.slice(0,16);
+			var time0 = Date.parse(currDate + res.timeStart);
+			var time1 = Date.parse(currDate + res.timeEnd);
+			var newTime = Date.parse(currTime);
+			if(res.onOff && (time0 <= newTime && newTime <= time1)){
+				contents.remove();
+			}else{
+				parent.prepend(contents);
+			}
+		});
 	}
 });
