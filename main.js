5ade725604c4b2b7bd518c6c26f42eff8e7cffac
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-11-07 14:34:01 -0500

    Added active time data to on/off extension activation

--- main.js
+++ main.js
@@ -7,9 +7,17 @@ var contents = $('#contents');
 var parent = $('#contents').parentElement;
 
 // Simple dynamic extension control 
-chrome.storage.local.get(['onOff'], function(res){
+chrome.storage.local.get(['onOff', 'timeStart', 'timeEnd'], function(res){
 	var onFlag  = res.onOff;
-	if(onFlag){
+
+	// Check active times
+	var currTime = new Date().toString();
+	var currDate = currTime.slice(0,16);
+	var time0 = Date.parse(currDate + res.timeStart);
+	var time1 = Date.parse(currDate + res.timeEnd);
+	var newTime = Date.parse(currTime);
+	
+	if(onFlag && (time0 <= newTime && newTime <= time1)){
 		contents.remove();
 	}else{
 		parent.prepend(contents);
