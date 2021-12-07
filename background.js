168c21f3fe65b5654218c70323b5de6cfeb8db51
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-12-07 17:46:21 -0500

    Added function to initialize the storage variables for the toggle switches

--- background.js
+++ background.js
@@ -2,7 +2,8 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log('onInstalled...');
   // create alarm after extension is installed / upgraded
 	chrome.alarms.create('refresh', { periodInMinutes: 1 });
-	setSiteData();
+	initializeSiteData();
+	initializeStorageData();
 });
 
 
@@ -36,7 +37,7 @@ chrome.webNavigation.onCompleted.addListener(() => {
 }, filter);
 
 
-function setSiteData(){
+function initializeSiteData(){
 	// Sites to block with their associated elements to block
 	var siteInfo = {
 		reddit: ['._31N0dvxfpsO6Ur5AKx4O5d', '#siteTable'],
@@ -46,10 +47,27 @@ function setSiteData(){
 	// Store site data to local memory
 	chrome.storage.sync.set({['siteData'] : siteInfo}, function(res){
 		if(!chrome.runtime.lastError){
-			// Set storage value successfully
+			// Set site data values successfully
 		}
 	});	
 }
+
+
+function initializeStorageData(){
+	const darkModeToggle = 'test';
+	const onOffToggle = 'onOfff';
+	chrome.storage.local.set({['test'] : false}, function(res){
+		if(!chrome.runtime.lastError){
+			// Set storage data successfully
+		}
+	});
+	chrome.storage.local.set({['onOff'] :  false}, function(res){
+		if(!chrome.runtime.lastError){
+			// Set storage data successfully
+		}
+	});
+}
+
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if(request) {
